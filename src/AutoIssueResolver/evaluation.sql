7a5ceebf193a518035258065643223568504447b
Jannis Pohle <git@jannispohle.de>
2025-07-25 06:07:22 +0200

    feat: Add script that can be used to evaluate an application run

---
+++ src/AutoIssueResolver/evaluation.sql
@@ -0,0 +1,25 @@
+SELECT
+    '(' ||
+    'numberOfFailedRequests: ' || IFNULL(SUM(CASE WHEN Status = 2 THEN 1 ELSE 0 END), 0) || ',' ||
+    'numberOfCompilationErrors: 0,' ||
+    'issuesWithCompilationErrors: [],' ||
+    'numberOfTestFailures: 0,' ||
+    'issuesWithTestFailures: [],' ||
+    'numberOfFixedIssues: 0,' ||
+    'totalNumberOfRemainingIssues: 0,' ||
+    'totalTokensUsed: ' || IFNULL(SUM(TotalTokensUsed), 0) || ',' ||
+    'inputTokensUsed: ' || IFNULL(SUM(PromptTokens), 0) || ',' ||
+    'outputTokensUsed: ' || IFNULL(SUM(ResponseTokens), 0) || ',' ||
+    'totalRunDurationInSeconds: ' || IFNULL((JULIANDAY(MAX(EndTimeUtc)) - JULIANDAY(MIN(StartTimeUtc))) * 86400.0, 0) || ',' ||
+    'totalNumberOfRetriedRequests: ' || IFNULL(SUM(CASE WHEN Retries > 0 THEN 1 ELSE 0 END), 0) || ',' ||
+    'totalNumberOfRetries: ' || IFNULL(SUM(Retries), 0) || ',' ||
+    'issuesWithRetries: [' ||
+    CASE
+        WHEN GROUP_CONCAT(CASE WHEN Retries > 0 THEN '"' || CodeSmellReference || '"' END) IS NULL THEN ''
+        ELSE GROUP_CONCAT(CASE WHEN Retries > 0 THEN '"' || CodeSmellReference || '"' END)
+        END
+        || ']' ||
+    ')'
+        AS testResults
+FROM Requests
+WHERE ApplicationRunId = '';
